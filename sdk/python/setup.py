82a7bdb21be3a17187762b1f6c9a8766aa594e37
naoki-maeda <infrontofden@gmail.com>
2023-04-28 12:30:34 +0900

    requirements.txtからパッケージを取得する

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -13,16 +13,8 @@ packages = \
 package_data = \
 {'': ['*']}
 
-install_requires = \
-['cryptography==38.0.1',
-    'mnemonic==0.20',
-    'Pillow==9.2.0',
-    'pynacl==1.5.0',
-    'pysha3==1.0.2',
-    'PyYAML==6.0',
-    'pyzbar==0.1.9',
-    'ripemd-hash==1.0.0',
-    'qrcode==7.3.1']
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup_kwargs = {
     'name': 'techbureau-symbol-sdk-python',
@@ -36,7 +28,7 @@ setup_kwargs = {
     'url': 'https://github.com/tech-bureau-jp/symbol/tree/dev/sdk/python',
     'packages': packages,
     'package_data': package_data,
-    'install_requires': install_requires,
+    'install_requires': requirements,
     'python_requires': '>=3.7,<4.0',
 }
 
